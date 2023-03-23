596890158e8555c3593134c7069c0b0b66adcc0e
Anna <119071299+heyanja@users.noreply.github.com>
2023-03-22 20:55:52 -0400

    Результат кода по 8 спринту

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Anna Fadieieva on 2023-03-08.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack")!}
+    static var ypGray: UIColor { UIColor(named: "ypGray")!}
+    static var ypBackground: UIColor { UIColor(named: "ypBackground")!}
+    static var ypWhite: UIColor { UIColor(named: "ypWhite")!}
+    static var ypWhiteAlpha50: UIColor { UIColor(named: "ypWhiteAlpha50")!}
+    static var ypRed: UIColor { UIColor(named: "ypRed")!}
+    static var ypBlue: UIColor { UIColor(named: "ypBlue")!}
+
+     }
