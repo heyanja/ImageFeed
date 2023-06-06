9af81168e861721f97f4a65034faf3d9f69f6a84
Anna <119071299+heyanja@users.noreply.github.com>
2023-06-05 21:17:59 -0400

    Добавлен progressView

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -11,6 +11,7 @@ protocol WebViewViewControllerDelegate: AnyObject {
 
 final class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
+    @IBOutlet private var progressView: UIProgressView!
     
     weak var delegate: WebViewViewControllerDelegate?
     
